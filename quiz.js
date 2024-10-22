132539c544955f23793cb5c96933adc2e27acf1c
Paulo Ricardo <122757615+pstationbr@users.noreply.github.com>
2024-10-22 17:56:30 -0300

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,15 @@
+// quizQuestions.js
+
+const quizQuestions = [
+    {
+        question: 'Quem construiu a Arca?',
+        answer: 'Noé'
+    },
+    {
+        question: 'Quantos discípulos Jesus tinha?',
+        answer: '12'
+    },
+    // Adicione mais perguntas aqui
+];
+
+module.exports = quizQuestions;
